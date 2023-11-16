a757ed9b66640018e8b0a9d3ff38d7d57b37414d
emmk9 <91438263+emmk9@users.noreply.github.com>
2023-11-16 19:48:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-$(document).ready(function () {
-	$('#courseForm').on('submit', function (e) {
-	  e.preventDefault();
+// Wait for the DOM to be ready before running any jQuery code
 
-	  const courseId = $('#courseId').val();
-	  const title = $('#title').val();
-	  let grade = $('#grade').val();
+$(document).ready(function() {
 
-	  // Input validation for grade
-	  
-	  grade = parseInt(grade);
-	  if (isNaN(grade) || grade < 0 || grade > 100) {
-		alert('Grade must be an integer between 0 and 100.');
-		return;
-	  }
+	// Event listener for form submission
 
-	  const row = `
+	$('#courseForm').on('submit', function(e) {
+
+		// Prevents the default form submission action
+
+		e.preventDefault();
+
+		// Retrieves values entered by the user in the form
+		
+		const courseId = $('#courseId').val();
+		const title = $('#title').val();
+		let grade = $('#grade').val();
+
+		// Validates the grade input
+
+		grade = parseInt(grade);
+		if (isNaN(grade) || grade < 0 || grade > 100) {
+			alert('Grade must be an integer between 0 and 100.');
+			return;
+		}
+
+		// Creates a new table row with the course information
+
+		const row = `
 		<tr>
 		  <td>${courseId}</td>
 		  <td>${title}</td>
@@ -23,55 +35,88 @@ $(document).ready(function () {
 		</tr>
 	  `;
 
-	  $('#courseTableBody').append(row);
+		// Appends the new row to the course table
 
-	  // Clear input fields
+		$('#courseTableBody').append(row);
 
-	  $('#courseId').val('');
-	  $('#title').val('');
-	  $('#grade').val('');
+		// Clears the form fields
+
+		$('#courseId').val('');
+		$('#title').val('');
+		$('#grade').val('');
 	});
 
-	$('#courseTableBody').on('click', '.removeBtn', function () {
-	  $(this).closest('tr').remove();
+	// Event listener for the Remove button in each course row
+
+	$('#courseTableBody').on('click', '.removeBtn', function() {
+		
+		// Removes the closest table row (the course row) when the Remove button is clicked
+		
+		$(this).closest('tr').remove();
 	});
 
-	// Sort grades function
+	// Function to sort the grades
 
 	function sortGrades() {
-	  const rows = $('#courseTableBody tr').get();
 
-	  rows.sort(function (a, b) {
-		const gradeA = parseInt($(a).children('td').eq(2).text());
-		const gradeB = parseInt($(b).children('td').eq(2).text());
+		// Retrieves all table rows
+		
+		const rows = $('#courseTableBody tr').get();
 
-		return gradeB - gradeA;
-	  });
+		// Sorts rows based on the grade values in descending order
 
-	  $.each(rows, function (index, row) {
-		$('#courseTableBody').append(row);
-	  });
+		rows.sort(function(a, b) {
+			const gradeA = parseInt($(a).children('td').eq(2).text());
+			const gradeB = parseInt($(b).children('td').eq(2).text());
+			return gradeB - gradeA;
+		});
+
+		// Re-adds the sorted rows to the table
+
+		$.each(rows, function(index, row) {
+			$('#courseTableBody').append(row);
+		});
 	}
 
-	// Sort button click event
+	// Event listener for the Sort button
 
-	$('#sortButton').on('click', function () {
-	  sortGrades();
+	$('#sortButton').on('click', function() {
+		sortGrades();
 	});
-  });
+});
 
 
 // Sticky Header
 
-window.onscroll = function() { makeSticky(); };
+// Attach makeSticky function to the window's scroll event
+
+window.onscroll = function() {
+	makeSticky();
+};
+
+// Get a reference to the HTML element with the ID "myHeader"
 
 var header = document.getElementById("myHeader");
+
+// Get the offset position of the header element
+
 var sticky = header.offsetTop;
 
+// Function to make the header sticky when scrolling
+
 function makeSticky() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+
+    // Check if the vertical scroll position (window.pageYOffset) is greater than the header's offset position (sticky)
+	
+	if (window.pageYOffset > sticky) {
+
+        // If it is, add the "sticky" class to the header element, making it sticky
+
+		header.classList.add("sticky");
+	} else {
+
+        // If it's not, remove the "sticky" class from the header element, making it non-sticky
+
+		header.classList.remove("sticky");
+	}
 }
